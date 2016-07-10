83233523582001dbe22d5a7816e8651057ef0018
Jonny Burger <jonathanburger11@gmail.com>
2016-07-10 23:46:49 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,45 @@
+# grade-statistics [![Build Status](https://travis-ci.org/JonnyBurger/grade-statistics.svg?branch=master)](https://travis-ci.org/JonnyBurger/grade-statistics)
+
+> My grand module
+
+
+## Install
+
+```
+$ npm install --save grade-statistics
+```
+
+
+## Usage
+
+```js
+const gradeStatistics = require('grade-statistics');
+
+gradeStatistics('unicorns');
+//=> 'unicorns & rainbows'
+```
+
+
+## API
+
+### gradeStatistics(input, [options])
+
+#### input
+
+Type: `string`
+
+Lorem ipsum.
+
+#### options
+
+##### foo
+
+Type: `boolean`<br>
+Default: `false`
+
+Lorem ipsum.
+
+
+## License
+
+MIT © [Jonny Burger](http://jonny.io)
---
+++ test.js
@@ -0,0 +1,65 @@
+import test from 'ava';
+import db from './lib/db';
+
+test.beforeEach(async () => {
+	await db.reset();
+	await db.createTables();
+});
+
+test('Should calculate stats correctly', async t => {
+	await db.insertStat('joburg', 50314925, 4, 'HS15');
+	await db.insertStat('jabond', 50314925, 4.5, 'HS15');
+	await db.insertStat('abcdef', 50314925, 3, 'HS15');
+	await db.insertStat('xyzabc', 50314925, 6, 'HS14');
+	await db.insertStat('xyzabd', 50314922, 5, 'HS15');
+
+	const result = await db.getStatsBySemester(50314925);
+	t.is(result.rows[0].semester, 'HS15');
+	t.is(result.rows[0].average, 3.83);
+	t.is(result.rows[0].passed, 2);
+	t.is(result.rows[0].failed, 1);
+	t.is(result.rows[0].count, 3);
+
+	t.is(result.rows[1].semester, 'HS14');
+	t.is(result.rows[1].average, 6);
+	t.is(result.rows[1].passed, 1);
+	t.is(result.rows[1].failed, 0);
+	t.is(result.rows[1].count, 1);
+
+	const result2 = await db.getStatsBySemester(50314922);
+	t.is(result2.rows[0].semester, 'HS15');
+	t.is(result2.rows[0].average, 5);
+	t.is(result2.rows[0].passed, 1);
+	t.is(result2.rows[0].failed, 0);
+	t.is(result2.rows[0].count, 1);
+});
+
+test('Should not allow two entries from the same person', async t => {
+	const result = await db.insertStat('joburg', 50314925, 4.5, 'HS15');
+	t.is(result.command, 'INSERT');
+
+	t.throws(db.insertStat('joburg', 50314925, 5, 'HS15'));
+});
+
+test('Should correctly insert statistics', async t => {
+	const statistic = {
+		source: 'oecnews Nr. 124',
+		points: [
+			{
+				module: 50030855,
+				comment: 'Betriebswirtschaftslehre II',
+				passed: 664,
+				failed: 308,
+				average: 4.14,
+				semester: 'FS14'
+			}
+		]
+	};
+	await db.insertPredefined(statistic);
+	await db.insertStat('joburg', 50030855, 6, 'FS15');
+	await db.insertStat('joburg', 50030855, 3.5, 'FS14');
+	await db.insertStat('joburg', 50030855, 6, 'FS13');
+	const stats = await db.getAllStats(50030855);
+	t.is(stats.total.passed, 666);
+	t.is(stats.total.average, 4.14);
+});
