d57bf4c1543601df22a55f25f5f8c33201abd984
Jonny Burger <jonathanburger11@gmail.com>
2016-07-11 00:33:25 +0200

    works now?

--- test/server.js
+++ test/server.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import test from 'ava';
+import fetch from 'node-fetch';
 import server from '../lib/server';
+
 var app = express();
 
 app.get('/', server.router);
 
 app.listen(2000);
+
+test('Server does respond', async t => {
+	let response = await fetch('http://localhost:2000').then(response => response.text());
+	t.is(response, 'ha');
+});
