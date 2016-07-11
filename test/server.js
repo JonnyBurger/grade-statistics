561e839f8f5ec88a5cff6debf733ec1c862bfaba
Jonny Burger <jonathanburger11@gmail.com>
2016-07-11 02:00:32 +0200

    Fix build

--- test/server.js
+++ test/server.js
@@ -2,14 +2,171 @@ import express from 'express';
 import test from 'ava';
 import fetch from 'node-fetch';
 import server from '../lib/server';
+import db from '../lib/db';
+
+test.beforeEach(async () => {
+	await db.reset();
+	await db.createTables();
+});
 
 var app = express();
-
-app.get('/', server.router);
-
+app.use('/', server.router);
 app.listen(2000);
 
-test('Server does respond', async t => {
-	let response = await fetch('http://localhost:2000').then(response => response.text());
-	t.is(response, 'ha');
+function doInsert(payload) {
+	return fetch('http://localhost:2000/', {
+		method: 'POST',
+		body: JSON.stringify(payload),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+}
+
+test('Should allow insertion of modules', async t => {
+	const payload = {
+		user: 'joburg',
+		grades: [
+			{
+				module: 50030855,
+				semester: 'FS15',
+				grade: 4
+			}
+		]
+	};
+	let postResponse = await doInsert(payload);
+	var postStatus = postResponse.status;
+	var postJson = await postResponse.json();
+	t.deepEqual(postJson, {
+		success: true
+	});
+	t.is(postStatus, 200);
+
+	let getResponse = await fetch(`http://localhost:2000/50030855`);
+	var getJson = await getResponse.json();
+	t.is(getResponse.status, 200);
+	t.is(getJson.total.passed, 1);
+	t.is(getJson.total.failed, 0);
+	t.is(getJson.total.average, 4);
+	t.is(getJson.detailed.length, 1);
+	t.is(getJson.success, true);
+});
+
+test('Should not allow insertion of grade bigger than 6', async t => {
+	const payload = {
+		user: 'joburg',
+		grades: [
+			{
+				module: 50030856,
+				semester: 'FS14',
+				grade: 7
+			}
+		]
+	};
+
+	let postResponse = await doInsert(payload);
+
+	t.is(postResponse.status, 400);
+});
+
+test('Should reject payload without username', async t => {
+	const badPayload = {
+		grades: [
+			{
+				module: 50030856,
+				semester: 'FS14',
+				grade: 5
+			}
+		]
+	};
+
+	let postResponse = await doInsert(badPayload);
+	t.is(postResponse.status, 400);
+});
+
+test('Should reject payload without semester', async t => {
+	const badPayload = {
+		user: 'joburg',
+		grades: [
+			{
+				module: 50030856,
+				grade: 5
+			}
+		]
+	};
+
+	let postResponse = await doInsert(badPayload);
+	t.is(postResponse.status, 400);
+});
+
+test('Should reject payload without module', async t => {
+	const badPayload = {
+		user: 'joburg',
+		grades: [
+			{
+				semester: 'FS14',
+				grade: 5
+			}
+		]
+	};
+
+	let postResponse = await doInsert(badPayload);
+	t.is(postResponse.status, 400);
+});
+
+test('Should reject empty payload', async t => {
+	const badPayload = {};
+
+	let postResponse = await doInsert(badPayload);
+	t.is(postResponse.status, 400);
+});
+
+test('Should reject invalid semester', async t => {
+	const payload = {
+		user: 'joburg',
+		grades: [
+			{
+				module: 50030856,
+				semester: 'bestande',
+				grade: 5
+			}
+		]
+	};
+
+	let postResponse = await doInsert(payload);
+	t.is(postResponse.status, 400);
+});
+
+test('Should not be able to overwrite semester', async t => {
+	const firstPayload = {
+		user: 'joburg',
+		grades: [
+			{
+				module: 50030855,
+				semester: 'FS15',
+				grade: 4
+			}
+		]
+	};
+
+	let firstResponse = await doInsert(firstPayload);
+	t.is(firstResponse.status, 200);
+
+	const secondPayload = {
+		user: 'joburg',
+		grades: [
+			{
+				module: 50030855,
+				semester: 'FS15',
+				grade: 5
+			}
+		]
+	};
+
+	let secondResponse = await doInsert(secondPayload);
+	t.is(secondResponse.status, 400);
+
+	let getResponse = await fetch('http://localhost:2000/50030855');
+	let getJson = await getResponse.json();
+	t.is(getJson.total.average, 4);
 });
