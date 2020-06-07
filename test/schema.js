88b43619ca8a21397e8c70f5c97717a7a4031f16
Jonny Burger <jonathanburger11@gmail.com>
2020-06-07 22:45:36 +0200

    fix test

--- test/schema.js
+++ test/schema.js
@@ -1,155 +1,204 @@
-import md5 from 'md5';
-import test from 'ava';
-import db from '../lib/db';
+const md5 = require("md5");
+const test = require("ava");
+const db = require("../lib/db");
 
 test.beforeEach(async () => {
 	await db.reset();
 	await db.createTables();
 });
 
-test('Should calculate stats correctly', async t => {
-	await db.insertStat(md5('joburg'), 50314925, 4, 'HS15', false, 'UZH');
-	await db.insertStat(md5('jabond'), 50314925, 4.5, 'HS15', false, 'UZH');
-	await db.insertStat(md5('abcdef'), 50314925, 3, 'HS15', false, 'UZH');
-	await db.insertStat(md5('xyzabc'), 50314925, 6, 'HS14', false, 'UZH');
-	await db.insertStat(md5('xyzabd'), 50314922, 5, 'HS15', false, 'UZH');
+test.serial("Should calculate stats correctly", async (t) => {
+	await db.insertStat(md5("joburg"), 50314925, 4, "HS15", false, "UZH");
+	await db.insertStat(md5("jabond"), 50314925, 4.5, "HS15", false, "UZH");
+	await db.insertStat(md5("abcdef"), 50314925, 3, "HS15", false, "UZH");
+	await db.insertStat(md5("xyzabc"), 50314925, 6, "HS14", false, "UZH");
+	await db.insertStat(md5("xyzabd"), 50314922, 5, "HS15", false, "UZH");
 
-	const result = await db.getStatsBySemester(50314925, 'UZH');
-	t.is(result.rows[0].semester, 'HS15');
+	const result = await db.getStatsBySemester(50314925, "UZH");
+	t.is(result.rows[0].semester, "HS15");
 	t.is(result.rows[0].average, 3.83);
 	t.is(result.rows[0].passed, 2);
 	t.is(result.rows[0].failed, 1);
 	t.is(result.rows[0].count, 3);
 
-	t.is(result.rows[1].semester, 'HS14');
+	t.is(result.rows[1].semester, "HS14");
 	t.is(result.rows[1].average, 6);
 	t.is(result.rows[1].passed, 1);
 	t.is(result.rows[1].failed, 0);
 	t.is(result.rows[1].count, 1);
 
-	const result2 = await db.getStatsBySemester(50314922, 'UZH');
-	t.is(result2.rows[0].semester, 'HS15');
+	const result2 = await db.getStatsBySemester(50314922, "UZH");
+	t.is(result2.rows[0].semester, "HS15");
 	t.is(result2.rows[0].average, 5);
 	t.is(result2.rows[0].passed, 1);
 	t.is(result2.rows[0].failed, 0);
 	t.is(result2.rows[0].count, 1);
 });
 
-test('Batch inserting should work correctly', async t => {
+test.serial("Batch inserting should work correctly", async (t) => {
 	await db.insertStats([
-		[md5('joburg'), 50314925, 4, 'HS15', false, 'UZH'],
-		[md5('joburg'), 50314926, 4, 'HS15', false, 'UZH'],
-		[md5('joburg'), 50314927, 4, 'HS15', false, 'UZH'],
-		[md5('joburg'), 50314928, 4, 'HS15', false, 'UZH']
+		[md5("joburg"), 50314925, 4, "HS15", false, "UZH"],
+		[md5("joburg"), 50314926, 4, "HS15", false, "UZH"],
+		[md5("joburg"), 50314927, 4, "HS15", false, "UZH"],
+		[md5("joburg"), 50314928, 4, "HS15", false, "UZH"],
 	]);
 	// some duplicates, some new
 	await db.insertStats([
-		[md5('joburg'), 50314925, 4, 'HS15', false, 'UZH'],
-		[md5('joburg'), 50314930, 4, 'HS15', false, 'UZH'],
-		[md5('joburg'), 50314931, 4, 'HS15', false, 'UZH'],
-		[md5('joburg'), 50314928, 4, 'HS15', false, 'UZH']
+		[md5("joburg"), 50314925, 4, "HS15", false, "UZH"],
+		[md5("joburg"), 50314930, 4, "HS15", false, "UZH"],
+		[md5("joburg"), 50314931, 4, "HS15", false, "UZH"],
+		[md5("joburg"), 50314928, 4, "HS15", false, "UZH"],
 	]);
 
-	const result = await db.getStatsBySemester(50314925, 'UZH');
+	const result = await db.getStatsBySemester(50314925, "UZH");
 	t.is(result.rows[0].count, 1);
-	const result2 = await db.getStatsBySemester(50314930, 'UZH');
+	const result2 = await db.getStatsBySemester(50314930, "UZH");
 	t.is(result2.rows[0].count, 1);
 });
 
-test('Should not allow two entries from the same person', async t => {
-	const result = await db.insertStat(md5('joburg'), 50314925, 4.5, 'HS15', false, 'UZH');
-	t.is(result.command, 'INSERT');
+test.serial("Should not allow two entries from the same person", async (t) => {
+	const result = await db.insertStat(
+		md5("joburg"),
+		50314925,
+		4.5,
+		"HS15",
+		false,
+		"UZH"
+	);
+	t.is(result.command, "INSERT");
 
-	await t.throws(db.insertStat(md5('joburg'), 50314925, 5, 'HS15', false, 'UZH'));
+	await t.throwsAsync(() =>
+		db.insertStat(md5("joburg"), 50314925, 5, "HS15", false, "UZH")
+	);
 });
 
-test('Should allow two entries if it is a Wiederholungsprüfung', async t => {
-	const result = await db.insertStat(md5('joburg'), 50314925, 4.5, 'HS15', false, 'UZH');
-	t.is(result.command, 'INSERT');
+test.serial(
+	"Should allow two entries if it is a Wiederholungsprüfung",
+	async (t) => {
+		const result = await db.insertStat(
+			md5("joburg"),
+			50314925,
+			4.5,
+			"HS15",
+			false,
+			"UZH"
+		);
+		t.is(result.command, "INSERT");
 
-	await t.notThrows(db.insertStat(md5('joburg'), 50314925, 5, 'HS15', true, 'UZH'));
-});
+		await t.notThrows(() =>
+			db.insertStat(md5("joburg"), 50314925, 5, "HS15", true, "UZH")
+		);
+	}
+);
 
-test('Should tell me if I am opted out', async t => {
-	const result = await db.studentIsOptedIn(md5('joburg'), 'UZH');
+test.serial("Should tell me if I am opted out", async (t) => {
+	const result = await db.studentIsOptedIn(md5("joburg"), "UZH");
 	t.is(result, false);
 });
 
-test('Should tell me if I am opted in', async t => {
-	await db.insertStat(md5('joburg'), 50314925, 4, 'HS15', false, 'UZH');
-	const result = await db.studentIsOptedIn(md5('joburg'), 'UZH');
+test.serial("Should tell me if I am opted in", async (t) => {
+	await db.insertStat(md5("joburg"), 50314925, 4, "HS15", false, "UZH");
+	const result = await db.studentIsOptedIn(md5("joburg"), "UZH");
 	t.is(result, true);
 });
 
-test('Should track an opt-out', async t => {
-	await db.addOptOut(md5('joburg'), 'UZH');
-	const optOuts = await db.studentOptOutCount(md5('joburg'), 'UZH');
+test.serial("Should track an opt-out", async (t) => {
+	await db.addOptOut(md5("joburg"), "UZH");
+	const optOuts = await db.studentOptOutCount(md5("joburg"), "UZH");
 	t.is(optOuts, 1);
 
-	await db.addOptOut(md5('joburg'), 'UZH');
-	const optOuts2 = await db.studentOptOutCount(md5('joburg'), 'UZH');
+	await db.addOptOut(md5("joburg"), "UZH");
+	const optOuts2 = await db.studentOptOutCount(md5("joburg"), "UZH");
 	t.is(optOuts2, 2);
 
-	await db.addOptOut(md5('joburg'), 'ETH');
-	const optOuts3 = await db.studentOptOutCount(md5('joburg'), 'ETH');
+	await db.addOptOut(md5("joburg"), "ETH");
+	const optOuts3 = await db.studentOptOutCount(md5("joburg"), "ETH");
 	t.is(optOuts3, 1);
 });
 
-test('Should correctly insert statistics', async t => {
+test.serial("Should correctly insert statistics", async (t) => {
 	const statistic = {
-		source: 'oecnews Nr. 124',
-		points: [
-			{
-				module: 50030855,
-				comment: 'Betriebswirtschaftslehre II',
-				passed: 664,
-				failed: 308,
-				average: 4.14,
-				semester: 'FS14'
-			}
-		],
-		institution: 'UZH'
+		source: "oecnews Nr. 124",
+		module: 50030855,
+		comment: "Betriebswirtschaftslehre II",
+		passed: 664,
+		failed: 308,
+		average: 4.14,
+		semester: "FS14",
+		institution: "UZH",
 	};
 	await db.insertPredefined(statistic);
-	await db.insertStat(md5('joburg'), 50030855, 6, 'FS15', false, 'UZH');
-	await db.insertStat(md5('joburg'), 50030855, 3.5, 'FS14', false, 'UZH');
-	await db.insertStat(md5('joburg'), 50030855, 6, 'FS13', false, 'UZH');
+	await db.insertStat(md5("joburg"), 50030855, 6, "FS15", false, "UZH");
+	await db.insertStat(md5("joburg"), 50030855, 3.5, "FS14", false, "UZH");
+	await db.insertStat(md5("joburg"), 50030855, 6, "FS13", false, "UZH");
 
-	const stats = await db.getAllStats(50030855, 'UZH');
+	const stats = await db.getAllStats(50030855, "UZH");
 	t.is(stats.total.passed, 666);
 	t.is(stats.total.average, 4.14);
 });
 
-test('Irrelevant statistic should not influence', async t => {
-	await db.insertPredefined({
-		source: 'Test statistic',
-		points: [
-			{
-				module: 123456,
-				comment: 'Test module',
-				passed: 1,
-				failed: 0,
-				average: 6,
-				semester: 'HS14'
-			}
-		],
-		institution: 'UZH'
-	});
-	await db.insertStat('da17d6edea42bf74fcd26015a9975030', 50044544, 4.25, 'HS14', false, 'UZH');
-	await db.insertStat('da17d6edea42bf74fcd26015a9975030', 50044544, 2.75, 'HS13', false, 'UZH');
-	await db.insertStat('c6042315ea9022d68a60bdac3bd07250', 50044544, 4.5, 'HS13', false, 'UZH');
+test.serial("Should work with more universities", async (t) => {
+	await new Promise((resolve) => setTimeout(resolve, 1000));
+	await db.insertStat(
+		"da17d6edea42bf74fcd26015a9975030",
+		50044544,
+		4.25,
+		"HS14",
+		false,
+		"UZH"
+	);
+	await db.insertStat(
+		"da17d6edea42bf74fcd26015a9975030",
+		50044544,
+		4.25,
+		"HS14",
+		false,
+		"ETH"
+	);
 
-	const stats = await db.getAllStats(50044544, 'UZH');
-
-	t.is(stats.total.passed, 2);
-});
-
-test('Should work with more universities', async t => {
-	await db.insertStat('da17d6edea42bf74fcd26015a9975030', 50044544, 4.25, 'HS14', false, 'UZH');
-	await db.insertStat('da17d6edea42bf74fcd26015a9975030', 50044544, 4.25, 'HS14', false, 'ETH');
-
-	const stats = await db.getAllStats(50044544, 'UZH');
-	const stats2 = await db.getAllStats(50044544, 'ETH');
+	const stats = await db.getAllStats(50044544, "UZH");
+	const stats2 = await db.getAllStats(50044544, "ETH");
 	t.is(stats.total.passed, 1);
 	t.is(stats2.total.passed, 1);
+});
+
+test.serial("Irrelevant statistic should not influence", async (t) => {
+	await db.insertPredefined({
+		source: "Test statistic",
+		module: 123456,
+		comment: "Test module",
+		passed: 1,
+		failed: 0,
+		average: 6,
+		semester: "HS14",
+		institution: "UZH",
+	});
+	await db.insertStat(
+		"da17d6edea42bf74fcd26015a9975030",
+		50044544,
+		4.25,
+		"HS14",
+		false,
+		"UZH"
+	);
+	await db.insertStat(
+		"da17d6edea42bf74fcd26015a9975030",
+		50044544,
+		2.75,
+		"HS13",
+		false,
+		"UZH"
+	);
+	await db.insertStat(
+		"c6042315ea9022d68a60bdac3bd07250",
+		50044544,
+		4.5,
+		"HS13",
+		false,
+		"UZH"
+	);
+
+	const stats = await db.getAllStats(50044544, "UZH");
+
+	t.is(stats.total.passed, 2);
 });
--- test/server.js
+++ test/server.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import md5 from 'md5';
-import test from 'ava';
-import fetch from 'node-fetch';
-import server from '../lib/server';
-import db from '../lib/db';
-import {MAX_OPTOUTS} from '../lib/config';
+const express = require("express");
+const md5 = require("md5");
+const test = require("ava");
+const fetch = require("node-fetch");
+const server = require("../lib/server");
+const db = require("../lib/db");
+const { MAX_OPTOUTS } = require("../lib/config");
 
 test.beforeEach(async () => {
 	await db.reset();
@@ -12,62 +12,62 @@ test.beforeEach(async () => {
 });
 
 var app = express();
-app.use('/', server.router);
+app.use("/", server.router);
 app.listen(2000);
 
 function doInsert(payload) {
-	return fetch('http://localhost:2000/', {
-		method: 'POST',
+	return fetch("http://localhost:2000/", {
+		method: "POST",
 		body: JSON.stringify(payload),
 		headers: {
-			'Content-Type': 'application/json'
-		}
+			"Content-Type": "application/json",
+		},
 	});
 }
 
 function doDelete(user) {
-	return fetch('http://localhost:2000', {
-		method: 'DELETE',
+	return fetch("http://localhost:2000", {
+		method: "DELETE",
 		body: JSON.stringify({
 			user: md5(user),
-			institution: 'UZH'
+			institution: "UZH",
 		}),
 		headers: {
-			'Content-Type': 'application/json'
-		}
+			"Content-Type": "application/json",
+		},
 	});
 }
 
 function doOptedInRequest(user) {
-	return fetch('http://localhost:2000/opted-in', {
-		method: 'POST',
+	return fetch("http://localhost:2000/opted-in", {
+		method: "POST",
 		body: JSON.stringify({
 			user: md5(user),
-			institution: 'UZH'
+			institution: "UZH",
 		}),
 		headers: {
-			'Content-Type': 'application/json'
-		}
+			"Content-Type": "application/json",
+		},
 	});
 }
 
-test('Should allow insertion of modules', async t => {
+test("Should allow insertion of modules", async (t) => {
 	const payload = {
-		user: md5('joburg'),
+		user: md5("joburg"),
 		grades: [
 			{
 				module: 50030855,
-				semester: 'FS15',
-				grade: 4
-			}
+				semester: "FS15",
+				grade: 4,
+			},
 		],
-		institution: 'UZH'
+		institution: "UZH",
 	};
 	let postResponse = await doInsert(payload);
 	var postStatus = postResponse.status;
 	var postJson = await postResponse.json();
 	t.deepEqual(postJson, {
-		success: true
+		success: true,
 	});
 	t.is(postStatus, 200);
 
@@ -81,23 +81,23 @@ test('Should allow insertion of modules', async t => {
 	t.is(getJson.success, true);
 });
 
-test('Should allow insertion of ETH modules', async t => {
+test("Should allow insertion of ETH modules", async (t) => {
 	const payload = {
-		user: md5('joburg'),
+		user: md5("joburg"),
 		grades: [
 			{
 				module: 50030855,
-				semester: 'FS15',
-				grade: 4
-			}
+				semester: "FS15",
+				grade: 4,
+			},
 		],
-		institution: 'ETH'
+		institution: "ETH",
 	};
 	let postResponse = await doInsert(payload);
 	var postStatus = postResponse.status;
 	var postJson = await postResponse.json();
 	t.deepEqual(postJson, {
-		success: true
+		success: true,
 	});
 	t.is(postStatus, 200);
 
@@ -116,17 +116,17 @@ test('Should allow insertion of ETH modules', async t => {
 	t.is(getJson2.total.passed, 0);
 });
 
-test('Should not allow insertion of grade bigger than 6', async t => {
+test("Should not allow insertion of grade bigger than 6", async (t) => {
 	const payload = {
-		user: md5('joburg'),
+		user: md5("joburg"),
 		grades: [
 			{
 				module: 50030856,
-				semester: 'FS14',
-				grade: 7
-			}
+				semester: "FS14",
+				grade: 7,
+			},
 		],
-		institution: 'UZH'
+		institution: "UZH",
 	};
 
 	let postResponse = await doInsert(payload);
@@ -134,186 +134,186 @@ test('Should not allow insertion of grade bigger than 6', async t => {
 	t.is(postResponse.status, 400);
 });
 
-test('Should reject payload without username', async t => {
+test("Should reject payload without username", async (t) => {
 	const badPayload = {
 		grades: [
 			{
 				module: 50030856,
-				semester: 'FS14',
-				grade: 5
-			}
+				semester: "FS14",
+				grade: 5,
+			},
 		],
-		institution: 'UZH'
+		institution: "UZH",
 	};
 
 	let postResponse = await doInsert(badPayload);
 	t.is(postResponse.status, 400);
 });
 
-test('Should reject payload without semester', async t => {
+test("Should reject payload without semester", async (t) => {
 	const badPayload = {
-		user: md5('joburg'),
+		user: md5("joburg"),
 		grades: [
 			{
 				module: 50030856,
-				grade: 5
-			}
+				grade: 5,
+			},
 		],
-		institution: 'UZH'
+		institution: "UZH",
 	};
 
 	let postResponse = await doInsert(badPayload);
 	t.is(postResponse.status, 400);
 });
 
-test('Should reject empty payload', async t => {
+test("Should reject empty payload", async (t) => {
 	const badPayload = {};
 
 	let postResponse = await doInsert(badPayload);
 	t.is(postResponse.status, 400);
 });
 
-test('Should reject invalid semester', async t => {
+test("Should reject invalid semester", async (t) => {
 	const payload = {
-		user: md5('joburg'),
+		user: md5("joburg"),
 		grades: [
 			{
 				module: 50030856,
-				semester: 'bestande',
-				grade: 5
-			}
+				semester: "bestande",
+				grade: 5,
+			},
 		],
-		institution: 'UZH'
+		institution: "UZH",
 	};
 
 	let postResponse = await doInsert(payload);
 	t.is(postResponse.status, 400);
 });
 
-test('Should not be able to overwrite semester', async t => {
+test("Should not be able to overwrite semester", async (t) => {
 	const firstPayload = {
-		user: md5('joburg'),
+		user: md5("joburg"),
 		grades: [
 			{
 				module: 50030855,
-				semester: 'FS15',
-				grade: 4
-			}
+				semester: "FS15",
+				grade: 4,
+			},
 		],
-		institution: 'UZH'
+		institution: "UZH",
 	};
 
 	let firstResponse = await doInsert(firstPayload);
 	t.is(firstResponse.status, 200);
 
 	const secondPayload = {
-		user: md5('joburg'),
+		user: md5("joburg"),
 		grades: [
 			{
 				module: 50030855,
-				semester: 'FS15',
-				grade: 5
-			}
+				semester: "FS15",
+				grade: 5,
+			},
 		],
-		institution: 'UZH'
+		institution: "UZH",
 	};
 
 	await doInsert(secondPayload);
 
-	let getResponse = await fetch('http://localhost:2000/50030855');
+	let getResponse = await fetch("http://localhost:2000/50030855");
 	let getJson = await getResponse.json();
 	t.is(getJson.total.average, 4);
 });
 
-test('Should be able to delete own grades', async t => {
+test("Should be able to delete own grades", async (t) => {
 	const firstPayload = {
-		user: md5('joburg'),
+		user: md5("joburg"),
 		grades: [
 			{
 				module: 50030855,
-				semester: 'FS15',
-				grade: 4
-			}
+				semester: "FS15",
+				grade: 4,
+			},
 		],
-		institution: 'UZH'
+		institution: "UZH",
 	};
 	let firstResponse = await doInsert(firstPayload);
 	t.is(firstResponse.status, 200);
 
-	let secondResponse = await doDelete('joburg');
+	let secondResponse = await doDelete("joburg");
 	t.is(secondResponse.status, 200);
 
-	let thirdResponse = await fetch('http://localhost:2000/50030855');
+	let thirdResponse = await fetch("http://localhost:2000/50030855");
 	let getJson = await thirdResponse.json();
 	t.is(getJson.total.passed, 0);
 	t.is(getJson.detailed.length, 0);
 });
 
-test('Should count opt-outs correctly', async t => {
+test("Should count opt-outs correctly", async (t) => {
 	const payload = {
-		user: md5('joburg'),
+		user: md5("joburg"),
 		grades: [
 			{
 				module: 50030855,
-				semester: 'FS15',
-				grade: 4
-			}
+				semester: "FS15",
+				grade: 4,
+			},
 		],
-		institution: 'UZH'
+		institution: "UZH",
 	};
 	await doInsert(payload);
-	await doDelete('joburg');
+	await doDelete("joburg");
 
 	await doInsert(payload);
-	await doDelete('joburg');
+	await doDelete("joburg");
 
-	let response = await doOptedInRequest('joburg');
+	let response = await doOptedInRequest("joburg");
 	let json = await response.json();
 	t.is(json.optedIn, false);
 	t.is(json.optOuts, 2);
 });
 
-test('Should not count opt-out if not opted in', async t => {
-	await doDelete('joburg');
-	let response = await doOptedInRequest('joburg');
+test("Should not count opt-out if not opted in", async (t) => {
+	await doDelete("joburg");
+	let response = await doOptedInRequest("joburg");
 	let json = await response.json();
 	t.is(json.optOuts, 0);
 });
 
-test('Should not allow opt-out after max opt-outs', async t => {
+test("Should not allow opt-out after max opt-outs", async (t) => {
 	const payload = {
-		user: md5('joburg'),
+		user: md5("joburg"),
 		grades: [
 			{
 				module: 50030855,
-				semester: 'FS15',
-				grade: 4
-			}
+				semester: "FS15",
+				grade: 4,
+			},
 		],
-		institution: 'UZH'
+		institution: "UZH",
 	};
 
 	for (let i = 0; i < MAX_OPTOUTS; i++) {
 		await doInsert(payload);
-		await doDelete('joburg');
+		await doDelete("joburg");
 	}
 
 	await doInsert(payload);
-	const response = await doDelete('joburg');
+	const response = await doDelete("joburg");
 	t.is(response.status, 400);
 });
 
-test('User should be a md5 hash', async t => {
+test("User should be a md5 hash", async (t) => {
 	const payload = {
-		user: 'joburg',
+		user: "joburg",
 		grades: [
 			{
 				module: 50030855,
-				semester: 'FS15',
-				grade: 4
-			}
+				semester: "FS15",
+				grade: 4,
+			},
 		],
-		institution: 'UZH'
+		institution: "UZH",
 	};
 	let response = await doInsert(payload);
 	let json = await response.json();
@@ -321,97 +321,91 @@ test('User should be a md5 hash', async t => {
 	t.regex(json.error, /md5/i);
 });
 
-test('Should tell me whether I am opted in', async t => {
+test("Should tell me whether I am opted in", async (t) => {
 	const payload = {
-		user: md5('joburg'),
+		user: md5("joburg"),
 		grades: [
 			{
 				module: 50030855,
-				semester: 'FS15',
-				grade: 4
-			}
+				semester: "FS15",
+				grade: 4,
+			},
 		],
-		institution: 'UZH'
+		institution: "UZH",
 	};
 	await doInsert(payload);
 
-	let response = await doOptedInRequest('joburg');
+	let response = await doOptedInRequest("joburg");
 	let json = await response.json();
 	t.is(json.optedIn, true);
 });
 
-test('Should handle Wiederholungsprüfungen', async t => {
+test("Should handle Wiederholungsprüfungen", async (t) => {
 	const payload = {
-		user: md5('joburg'),
+		user: md5("joburg"),
 		grades: [
-			{semester: 'HS15', module: '50335165', grade: 4.5},
-			{semester: 'HS15', module: '50332398', grade: 4},
-			{semester: 'HS15', module: '50335165', grade: 3}
+			{ semester: "HS15", module: "50335165", grade: 4.5 },
+			{ semester: "HS15", module: "50332398", grade: 4 },
+			{ semester: "HS15", module: "50335165", grade: 3 },
 		],
-		institution: 'UZH'
+		institution: "UZH",
 	};
 
 	let response = await doInsert(payload);
 	t.is(response.status, 200);
 });
 
-test('Should allow to insert new grades', async t => {
+test("Should allow to insert new grades", async (t) => {
 	const payload = {
-		user: md5('joburg'),
-		grades: [
-			{semester: 'HS15', module: '50335165', grade: 4}
-		],
-		institution: 'UZH'
+		user: md5("joburg"),
+		grades: [{ semester: "HS15", module: "50335165", grade: 4 }],
+		institution: "UZH",
 	};
 
 	let response = await doInsert(payload);
 	t.is(response.status, 200);
 
 	const payload2 = {
-		user: md5('joburg'),
+		user: md5("joburg"),
 		grades: [
-			{semester: 'HS15', module: '50335165', grade: 4},
-			{semester: 'FS16', module: '50332398', grade: 5}
+			{ semester: "HS15", module: "50335165", grade: 4 },
+			{ semester: "FS16", module: "50332398", grade: 5 },
 		],
-		institution: 'UZH'
+		institution: "UZH",
 	};
 
 	let response2 = await doInsert(payload2);
 	t.is(response2.status, 200);
 
-	let module1 = await fetch('http://localhost:2000/50335165');
+	let module1 = await fetch("http://localhost:2000/50335165");
 	let json1 = await module1.json();
 
 	t.is(json1.total.count, 1);
 	t.is(json1.total.average, 4);
 
-	let module2 = await fetch('http://localhost:2000/50332398');
+	let module2 = await fetch("http://localhost:2000/50332398");
 	let json2 = await module2.json();
 
 	t.is(json2.total.count, 1);
 	t.is(json2.total.average, 5);
 });
 
-test('Should reject hash of empty string', async t => {
+test("Should reject hash of empty string", async (t) => {
 	const payload = {
-		user: md5(''),
-		grades: [
-			{semester: 'HS15', module: '50332398', grade: 4}
-		],
-		institution: 'UZH'
+		user: md5(""),
+		grades: [{ semester: "HS15", module: "50332398", grade: 4 }],
+		institution: "UZH",
 	};
 
 	let response = await doInsert(payload);
 	t.is(response.status, 400);
 });
 
-test('Should reject hash of demo account', async t => {
+test("Should reject hash of demo account", async (t) => {
 	const payload = {
-		user: md5('bestande'),
-		grades: [
-			{semester: 'HS15', module: '50332398', grade: 4}
-		],
-		institution: 'UZH'
+		user: md5("bestande"),
+		grades: [{ semester: "HS15", module: "50332398", grade: 4 }],
+		institution: "UZH",
 	};
 
 	let response = await doInsert(payload);
